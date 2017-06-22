327d5a32d2e9058daf0fb9dc5a374ad666e3cb75
William Goulart <william@youzoom.com.br>
2017-06-22 17:29:48 -0400

    Update package.json & gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,15 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync');
 var sass        = require('gulp-sass');
 var prefix      = require('gulp-autoprefixer');
+var merge       = require('merge-stream');
+var imagemin    = require('gulp-imagemin');
+var ghPages     = require('gulp-gh-pages');
 
 // Start Static Server with browserSync
 gulp.task('server', ['sass'], function() {
     browserSync({
         server: {
-            baseDir: "./"
+            baseDir: "dist/"
         }
     });
 });
@@ -25,6 +28,33 @@ gulp.task('sass', function () {
 gulp.task('watch', function () {
     gulp.watch('./_sass/**/*.scss', ['sass']);
     gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// Task to build to dist
+gulp.task('build', () => {
+    const task = () => {
+        let markup =  gulp.src('*.html')
+    		.pipe(gulp.dest('dist/'));
+
+        let styles = gulp.src('assets/css/main.css')
+            .pipe(gulp.dest('dist/css/'));
+
+        let images =  gulp.src('assets/images/**/')
+    	    .pipe(imagemin())
+    		.pipe(gulp.dest('dist/images/'));
+
+        return merge(markup ,styles, images);
+    }
+
+    task();
+});
+
+// Deploy build dist to branch gh-pages
+gulp.task('deploy', function() {
+    return gulp.src('./dist/**/*')
+        .pipe(ghPages({
+            force: true
+        }));
 });
 
 // Default task, running just `gulp`
