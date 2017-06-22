89865ded4db4fea3b237ec34c768109ccc02b896
William Goulart <william@youzoom.com.br>
2017-06-22 17:32:27 -0400

    Add requires and tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync');
-var sass        = require('gulp-sass');
-var prefix      = require('gulp-autoprefixer');
-var merge       = require('merge-stream');
-var imagemin    = require('gulp-imagemin');
-var ghPages     = require('gulp-gh-pages');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var merge = require('merge-stream');
+var imagemin = require('gulp-imagemin');
+var ghPages = require('gulp-gh-pages');
 
 // Start Static Server with browserSync
 gulp.task('server', ['sass'], function() {
@@ -16,16 +16,22 @@ gulp.task('server', ['sass'], function() {
 });
 
 // Watch SCSS files for changes, recompile, compress, autoprefixed
-gulp.task('sass', function () {
+gulp.task('sass', function() {
     return gulp.src('./_sass/main.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(prefix(['last 2 versions'], { cascade: true }))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(prefix(['last 2 versions'], {
+            cascade: true
+        }))
         .pipe(gulp.dest('./assets/css'))
-        .pipe(browserSync.reload({stream:true}));
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 // Watch SCSS & HTML files, run reload BrowserSync
-gulp.task('watch', function () {
+gulp.task('watch', function() {
     gulp.watch('./_sass/**/*.scss', ['sass']);
     gulp.watch("./*.html").on('change', browserSync.reload);
 });
@@ -33,17 +39,17 @@ gulp.task('watch', function () {
 // Task to build to dist
 gulp.task('build', () => {
     const task = () => {
-        let markup =  gulp.src('*.html')
-    		.pipe(gulp.dest('dist/'));
+        let markup = gulp.src('*.html')
+            .pipe(gulp.dest('dist/'));
 
         let styles = gulp.src('assets/css/main.css')
             .pipe(gulp.dest('dist/css/'));
 
-        let images =  gulp.src('assets/images/**/')
-    	    .pipe(imagemin())
-    		.pipe(gulp.dest('dist/images/'));
+        let images = gulp.src('assets/images/**/')
+            .pipe(imagemin())
+            .pipe(gulp.dest('dist/images/'));
 
-        return merge(markup ,styles, images);
+        return merge(markup, styles, images);
     }
 
     task();
